281296829dd4690871ef366eb362834e0d228a48
Fauzan <fauzan.c4@gmail.com>
2025-07-24 03:19:20 +0700

    feat: setup express server with routing and error handler

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, {Express, Request, Response, NextFunction} from "express"
+import mainRouter from "./routers/index.router"
+import cors from "cors"
+
+const app: Express = express()
+app.use(express.json())
+app.use(cors())
+const port = 4000
+
+app.use(mainRouter)
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({
+    success: false,
+    message: error?.isExpose ? error?.message : 'Internal server error'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`⚡️ Server is running on port ${port}`)
+})
