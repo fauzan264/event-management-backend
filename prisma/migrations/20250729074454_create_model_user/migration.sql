d8ae47d0356a750c305a520b1d5138d498e2d226
Fauzan <fauzan.c4@gmail.com>
2025-07-29 14:47:58 +0700

    feat: create model User

---
+++ prisma/migrations/20250729074454_create_model_user/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "id_card_number" TEXT NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "date_of_birth" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "referral_code" TEXT NOT NULL,
+    "total_user_point" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_card_number_key" ON "User"("id_card_number");
