856b6cca647a60ac7998007ee7b0969dbcaec1db
Fauzan <fauzan.c4@gmail.com>
2025-08-04 05:09:11 +0700

    feat: create endpoint update user

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { updateUserService } from "../services/user.service";
+
+export const updateUserController = async (req: Request, res: Response) => {
+  const {
+    id_card_number,
+    fullname,
+    date_of_birth,
+    email,
+    phone_number,
+  } = req.body
+
+  const { id } = req.params
+
+  const { userId } = res.locals.payload
+
+  const profilePictures = Array.isArray(req?.files)
+    ? req.files
+    : req.files
+    ?  (req.files as Record<string, Express.Multer.File[]>).profile_picture || []
+    : []
+
+  const user = await updateUserService({
+    id,
+    idCardNumber: id_card_number,
+    fullName: fullname,
+    dateOfBirth: date_of_birth,
+    email,
+    phoneNumber: phone_number,
+    profilePictures,
+    userId
+  })
+
+  return res.status(200).json({
+    status: true,
+    message: 'User updated successfully.',
+    data: user
+  })
+}
