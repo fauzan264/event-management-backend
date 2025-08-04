fbcf39b54c0d558b9a667ab2bf420fd891c60c07
Fauzan <fauzan.c4@gmail.com>
2025-08-05 03:09:26 +0700

    fix: combine endpoint generate referral code to auth register

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -18,81 +18,91 @@ export const authRegisterService = async ({
   referralCode,
   userRole
 }: Omit<User, 'id' | 'profilePicture' | 'totalUserPoint' | 'createdAt'| 'updatedAt' | 'deletedAt'>) => {
-  return await prisma.$transaction(async(tx) => {
+  try {
+    return await prisma.$transaction(async(tx) => {
     
-    let referral: User | null = null
-    const rewardPoint = 10000
-    if (referralCode) {
-      referral = await tx.user.findFirst({
-        where: {
-          referralCode
+      let referral: User | null = null
+      const rewardPoint = 10000
+      if (referralCode) {
+        referral = await tx.user.findFirst({
+          where: {
+            referralCode
+          }
+        })
+    
+        if (!referral) {
+          throw { message: `referral code ${referralCode} is not found`, isExpose: true }
+        }
+  
+        referral.totalUserPoint += rewardPoint
+      }
+    
+      const saltRound = 10
+      const hashedPassword = await bcrypt.hash(password, saltRound)
+    
+      const totalUserPoint = 0
+  
+      const createdUser = await tx.user.create({
+        data: {
+          idCardNumber,
+          fullName,
+          dateOfBirth: new Date(dateOfBirth),
+          email,
+          password: hashedPassword,
+          phoneNumber,
+          referralCode: generateCode("REF-"),
+          totalUserPoint,
+          userRole
         }
       })
+    
+      await jwtSign(
+        { userId: createdUser?.id },
+        process.env.JWT_SECRET_KEY!,
+        { algorithm: "HS256" }
+      )
   
-      if (!referral) {
-        throw { message: `referral code ${referralCode} is not found`, isExpose: true }
+      if (referral) {
+        const now = DateTime.now().setZone('Asia/Jakarta')
+        const threeMonthLater = now.plus({ months: 3 })
+  
+        await tx.user.update({
+          data: {
+            totalUserPoint: referral?.totalUserPoint
+          },
+          where: {
+            id: referral?.id
+          }
+        })
+  
+        await tx.userPoint.create({
+          data: {
+            userId: referral?.id,
+            points: rewardPoint,
+            expiredAt: threeMonthLater.toISO()!,
+          }
+        })
       }
-
-      referral.totalUserPoint += rewardPoint
-    }
   
-    const saltRound = 10
-    const hashedPassword = await bcrypt.hash(password, saltRound)
-  
-    const totalUserPoint = 0
-
-    const createdUser = await tx.user.create({
-      data: {
-        idCardNumber,
-        fullName,
-        dateOfBirth: new Date(dateOfBirth),
-        email,
-        password: hashedPassword,
-        phoneNumber,
-        totalUserPoint,
-        userRole
+      if (userRole == UserRole.EVENT_ORGANIZER) {
+        await tx.eventOrganizer.create({
+          data: {
+            companyName: `${fullName} company`,
+            email: email,
+            userId: createdUser.id
+          }
+        })
       }
     })
-  
-    await jwtSign(
-      { userId: createdUser?.id },
-      process.env.JWT_SECRET_KEY!,
-      { algorithm: "HS256" }
-    )
-
-    if (referral) {
-      const now = DateTime.now().setZone('Asia/Jakarta')
-      const threeMonthLater = now.plus({ months: 3 })
-
-      await tx.user.update({
-        data: {
-          totalUserPoint: referral?.totalUserPoint
-        },
-        where: {
-          id: referral?.id
-        }
-      })
-
-      await tx.userPoint.create({
-        data: {
-          userId: referral?.id,
-          points: rewardPoint,
-          expiredAt: threeMonthLater.toISO()!,
-        }
-      })
+  } catch (error: any) {
+    if (error?.code === "P2002" ) {
+      if (error?.meta?.target.includes("id_card_number")) {
+        throw { message: "ID Card Number already exists", isExpose: true }
+      } else if (error?.meta?.target.includes("email")) {
+        throw { message: "Email already exists", isExpose: true }
+      }
     }
-
-    if (userRole == UserRole.EVENT_ORGANIZER) {
-      await tx.eventOrganizer.create({
-        data: {
-          companyName: `${fullName} company`,
-          email: email,
-          userId: createdUser.id
-        }
-      })
-    }
-  })
-
+  }
 }
 
 export const authLoginService = async ({email, password}: Pick<User, 'email' | 'password'>) => {
@@ -113,29 +123,6 @@ export const authLoginService = async ({email, password}: Pick<User, 'email' | '
   )
 
   return { token, fullName: findUserByEmail?.fullName }
-}
-
-export const authGenenerateCodeReferralService = async ({ id }: Pick<User, 'id'>) => {
-  const findUserById = await prisma.user.findFirst({
-    where: { id }
-  })
-
-  if (!findUserById)
-    throw { message: `User with id = ${id} is not found`, isExpose: true }
-
-  if (findUserById.referralCode)
-    throw { message: `Referral code already exists and cannot be regenerated.`, isExpose: true }
-  
-  const createReferralCode = await prisma.user.update({
-    data: {
-      referralCode: generateCode("REF-")
-    },
-    where: {
-      id
-    }
-  })
-
-  return createReferralCode.referralCode
 }
 
 export const authRequestResetPasswordService = async ({email}: Pick<User, 'email'>) => {
