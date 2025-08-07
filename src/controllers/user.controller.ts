862d5abc48666347992a4a31fc4e188dab2e588d
Fauzan <fauzan.c4@gmail.com>
2025-08-08 03:06:28 +0700

    feat: create endpoint my event organizer

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,24 +1,22 @@
 import { Request, Response } from "express";
-import { updateUserService } from "../services/user.service";
+import {
+  getMyEventOrganizerService,
+  updateUserService,
+} from "../services/user.service";
 
 export const updateUserController = async (req: Request, res: Response) => {
-  const {
-    id_card_number,
-    fullname,
-    date_of_birth,
-    email,
-    phone_number,
-  } = req.body
+  const { id_card_number, fullname, date_of_birth, email, phone_number } =
+    req.body;
 
-  const { id } = req.params
+  const { id } = req.params;
 
-  const { userId } = res.locals.payload
+  const { userId } = res.locals.payload;
 
   const profilePictures = Array.isArray(req?.files)
     ? req.files
     : req.files
-    ?  (req.files as Record<string, Express.Multer.File[]>).profile_picture || []
-    : []
+    ? (req.files as Record<string, Express.Multer.File[]>).profile_picture || []
+    : [];
 
   const user = await updateUserService({
     id,
@@ -28,12 +26,28 @@ export const updateUserController = async (req: Request, res: Response) => {
     email,
     phoneNumber: phone_number,
     profilePictures,
-    userId
-  })
+    userId,
+  });
 
   return res.status(200).json({
     status: true,
-    message: 'User updated successfully.',
-    data: user
-  })
-}
+    message: "User updated successfully.",
+    data: user,
+  });
+};
+
+export const getMyEventOrganizerController = async (
+  req: Request,
+  res: Response
+) => {
+  const { id } = req.params;
+  const { userId } = res.locals.payload;
+
+  const eventOrganizer = await getMyEventOrganizerService({ id, userId });
+
+  return res.status(200).json({
+    status: true,
+    message: "Successfully fetched your event organizer data.",
+    res: eventOrganizer,
+  });
+};
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,25 @@
 import { Router } from "express";
 import { jwtVerify } from "../middleware/jwt.verify";
-import { updateUserController } from "../controllers/user.controller";
+import {
+  getMyEventOrganizerController,
+  updateUserController,
+} from "../controllers/user.controller";
 import { uploaderMulter } from "../middleware/uploader.multer";
 
-const userRouter = Router()
+const userRouter = Router();
 
-userRouter.put('/:id', uploaderMulter(['image'], 'memoryStorage').fields([{name: 'profile_picture', maxCount: 1}]), jwtVerify, updateUserController)
+userRouter.put(
+  "/:id",
+  uploaderMulter(["image"], "memoryStorage").fields([
+    { name: "profile_picture", maxCount: 1 },
+  ]),
+  jwtVerify,
+  updateUserController
+);
+userRouter.get(
+  "/:id/event-organizer",
+  jwtVerify,
+  getMyEventOrganizerController
+);
 
-export default userRouter
+export default userRouter;
