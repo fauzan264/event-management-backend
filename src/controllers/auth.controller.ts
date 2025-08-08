1c285fab914a0ada30231fb5e7ed5fabb37fec48
Fauzan <fauzan.c4@gmail.com>
2025-08-08 21:48:28 +0700

    fix: update auth session add user id and change response getMyEventOrganizer data res to data

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,12 @@
-import { Request, Response } from "express"
-import { authChangePasswordService, authLoginService, authRegisterService, authRequestResetPasswordService, authResetPasswordService, authSessionLoginService } from "../services/auth.service"
+import { Request, Response } from "express";
+import {
+  authChangePasswordService,
+  authLoginService,
+  authRegisterService,
+  authRequestResetPasswordService,
+  authResetPasswordService,
+  authSessionLoginService,
+} from "../services/auth.service";
 
 export const authRegisterController = async (req: Request, res: Response) => {
   const {
@@ -10,8 +17,8 @@ export const authRegisterController = async (req: Request, res: Response) => {
     password,
     phone_number,
     referral_code,
-    user_role
-  } = req.body
+    user_role,
+  } = req.body;
 
   await authRegisterService({
     idCardNumber: id_card_number,
@@ -21,74 +28,90 @@ export const authRegisterController = async (req: Request, res: Response) => {
     password: password,
     phoneNumber: phone_number,
     referralCode: referral_code,
-    userRole: user_role
-  })
+    userRole: user_role,
+  });
 
   res.status(201).json({
     success: true,
-    message: 'Register user successful',
+    message: "Register user successful",
     data: {
       fullname,
-      email
-    }
-  })
-}
+      email,
+    },
+  });
+};
 
 export const authLoginController = async (req: Request, res: Response) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
-  const { token, fullName, role } = await authLoginService({ email, password })
+  const { token, fullName, role } = await authLoginService({ email, password });
 
   res.status(200).json({
     success: true,
     message: `Login user successful`,
-    data: { token, full_name: fullName, role }
-  })
-}
+    data: { token, full_name: fullName, role },
+  });
+};
 
-export const authRequestResetPasswordController = async (req: Request, res: Response) => {
-  const { email } = req.body
+export const authRequestResetPasswordController = async (
+  req: Request,
+  res: Response
+) => {
+  const { email } = req.body;
 
-  await authRequestResetPasswordService({email})
-
-  res.status(200).json({
-    success: true,
-    message: "Password reset link has been sent to your email address."
-  })
-}
-
-export const authResetPasswordController = async (req: Request, res: Response) => {
-  const { password } = req.body
-  const { userId } = res.locals.payload
-
-  await authResetPasswordService({ id: userId, password })
+  await authRequestResetPasswordService({ email });
 
   res.status(200).json({
     success: true,
-    message: 'Password updated successfully'
-  })
-}
+    message: "Password reset link has been sent to your email address.",
+  });
+};
 
-export const authSessionLoginController = async (req: Request, res: Response) => {
-  const { userId } = res.locals.payload
+export const authResetPasswordController = async (
+  req: Request,
+  res: Response
+) => {
+  const { password } = req.body;
+  const { userId } = res.locals.payload;
 
-  const { role, fullname } = await authSessionLoginService({ id: userId })
-
-  res.status(200).json({
-    success: true,
-    message: 'Session data retrieved successfully.',
-    data: { fullname, role }
-  })
-}
-
-export const authChangePasswordController = async (req: Request, res: Response) => {
-  const { old_password, new_password } = req.body
-  const { userId } = res.locals.payload
-
-  await authChangePasswordService({oldPassword: old_password, newPassword: new_password, userId})
+  await authResetPasswordService({ id: userId, password });
 
   res.status(200).json({
     success: true,
-    message: 'Password changed successfully'
-  })
-}
+    message: "Password updated successfully",
+  });
+};
+
+export const authSessionLoginController = async (
+  req: Request,
+  res: Response
+) => {
+  const { userId } = res.locals.payload;
+
+  const { role, fullname } = await authSessionLoginService({ id: userId });
+
+  res.status(200).json({
+    success: true,
+    message: "Session data retrieved successfully.",
+    data: { id: userId, fullname, role },
+  });
+};
+
+export const authChangePasswordController = async (
+  req: Request,
+  res: Response
+) => {
+  const { old_password, new_password } = req.body;
+  const { userId } = res.locals.payload;
+
+  await authChangePasswordService({
+    oldPassword: old_password,
+    newPassword: new_password,
+    userId,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Password changed successfully",
+  });
+};
