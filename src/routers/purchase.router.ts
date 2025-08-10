1b9f9c8fefff2c06c4e9d9e4adf20fd3425e7ac2
Alwi Akbar <alwiakbar09@gmail.com>
2025-08-10 15:09:23 +0700

    feat:create orders by user API

--- src/routers/purchase.router.ts
+++ src/routers/purchase.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {expiredOrdersController, getAllOrderController, getOrderDetailController, purchaseOrderController} from '../controllers/purchase.controller'
+import {expiredOrdersController, getAllOrderController, getOrderbyUserIdController, getOrderDetailController, purchaseOrderController} from '../controllers/purchase.controller'
 import { jwtVerify } from "../middleware/jwt.verify";
 
 
 const purchaseRouter = Router()
 
 purchaseRouter.get('/orders', getAllOrderController);
+purchaseRouter.get('/orders/:userId', jwtVerify, getOrderbyUserIdController);
 purchaseRouter.get('/:orderId', getOrderDetailController);
 purchaseRouter.post('/:eventId', jwtVerify, purchaseOrderController);
 purchaseRouter.patch('/:eventId', expiredOrdersController);
